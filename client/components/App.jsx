4c28208b871eb2d681f8c74d84a017428ecef843
Magnus Havgry <magnushavgry@gmail.com>
2018-04-12 14:39:46 +0200

    Setup Webpack to restore sanity. Add ESLint (airbnb preset), resolve `.jsx` files, and add `webpack-notifier`

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,16 +1,10 @@
-import React from 'react';
+import React from 'react'
 
-class App extends React.Component {
-  
-  render() {
-    return (
-      <div>
-        <h1>Hello, World!</h1>
-        <p>Make something awesome!</p>
-      </div>
-    );
-  }
+const App = () => (
+  <div>
+    <h1>Hello, World!</h1>
+    <p>Make something awesome!</p>
+  </div>
+)
 
-}
-
-export default App;
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,38 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackNotifierPlugin = require('webpack-notifier')
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './client/index.html',
   filename: 'index.html',
-  inject: 'body'
+  inject: 'body',
 })
 
 module.exports = {
-  entry: './client/index.js',
+  entry: './client/index.jsx',
   output: {
     path: path.resolve('dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.(js|jsx)$/,
+        loader: ['babel-loader', 'eslint-loader'],
+        exclude: /node_modules/,
+      },
       {
         test: /\.scss$/,
-        exclude: /node_modules/,
         loaders: ['style-loader', 'css-loader', 'sass-loader'],
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
   },
-  plugins: [HtmlWebpackPluginConfig]
+  plugins: [
+    HtmlWebpackPluginConfig,
+    new WebpackNotifierPlugin(),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 }
